a1df2715a4c514f5938bb92c6245cbf95361ff98
Rocky <rocky.huang.dev@gmail.com>
2024-09-08 17:34:04 +0800

    feat: roughly implement request api

---
+++ core/domain/src/main/java/com/rocky/core/domain/run/RemoteRunDataSource.kt
@@ -0,0 +1,11 @@
+package com.rocky.core.domain.run
+
+import com.rocky.core.domain.util.DataError
+import com.rocky.core.domain.util.EmptyResult
+import com.rocky.core.domain.util.Result
+
+interface RemoteRunDataSource {
+    suspend fun getRuns(): Result<List<Run>, DataError.Network>
+    suspend fun postRun(run: Run, mapPicture: ByteArray): Result<Run, DataError.Network>
+    suspend fun deleteRun(id: String): EmptyResult<DataError.Network>
+}
