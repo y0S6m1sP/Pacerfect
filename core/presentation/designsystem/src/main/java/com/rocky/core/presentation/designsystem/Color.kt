b6a05fbaf905c10b588e3fd6fe6c2e53c3c66508
Rocky <rocky.huang.dev@gmail.com>
2024-09-13 20:21:34 +0800

    style: update color

--- core/presentation/designsystem/src/main/java/com/rocky/core/presentation/designsystem/Color.kt
+++ core/presentation/designsystem/src/main/java/com/rocky/core/presentation/designsystem/Color.kt
@@ -2,10 +2,10 @@ package com.rocky.core.presentation.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-val PacerfectBlue = Color(0xFF0000F1)
-val PacerfectBlue30 = Color(0x4D0000F1)
-val PacerfectBlue10 = Color(0x1A0000F1)
-val PacerfectBlue5 = Color(0x0D0000F1)
+val PacerfectBlue = Color(0xFF2196F3)
+val PacerfectBlue30 = Color(0x4D2196F3)
+val PacerfectBlue10 = Color(0x1A2196F3)
+val PacerfectBlue5 = Color(0x0D2196F3)
 val PacerfectBlack = Color(0xFF080707)
 val PacerfectGray = Color(0xFF87938C)
 val PacerfectGray40 = Color(0x6687938C)
